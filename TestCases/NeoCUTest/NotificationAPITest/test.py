cb89ff39c2cf50620c9edda29504dc4eef627e4e
zuoran <zuoran@huayun.com>
2018-05-28 10:36:11 +0800

    update

--- TestCases/NeoCUTest/NotificationAPITest/test.py
+++
@@ -1,8 +0,0 @@
-# encoding=utf-8
-# @Time    : 2017/10/30 9:55
-# @Author  : Zuo Ran
-# @File    : test.py
-
-from Utils.Property import Property
-
-Property.get_file("a", "b", "c")
--- TestCases/NeoCUTest/__init__.py
+++
@@ -1,4 +0,0 @@
-# encoding=utf-8
-# @Time    : 2017/10/26 11:43
-# @Author  : Zuo Ran
-# @File    : __init__.py.py
